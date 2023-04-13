2b0936cd540ab14aa7d09b8c9df1bff5cc6568cb
Jorge Olivera <jorge.olivera@istea.com.ar>
2023-04-12 23:49:40 -0300

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ProyectoPrimerParcial.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
