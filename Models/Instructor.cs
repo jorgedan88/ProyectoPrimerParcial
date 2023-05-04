3bd02c209a70d197d66bde57202cef6d128a12e1
jorgedan88 <129257560+jorgedan88@users.noreply.github.com>
2023-05-03 21:37:47 -0300

    Se agrega el modelo Instructor junto a sus validaciones

---
+++ Models/Instructor.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoPrimerParcial.Models
+{  
+    public class Instructor
+    {
+        public int Id{get; set;}
+
+        [Range(1,50000,ErrorMessage ="El campo debe ser un número entre 1 y 5000")]
+        [Required(ErrorMessage ="Debe ingresar el Legajo de Vuelo")]
+        public int LegajoVuelo {get; set;}
+        
+        [Required(ErrorMessage ="Debe ingresar el Nombre")]
+        public string Nombre {get; set;}
+
+        [Required(ErrorMessage ="Debe ingresar el Apellido")]
+        public string Apellido {get; set;}
+
+        [Required(ErrorMessage ="Debe ingresar el DNI")]
+        public int DNI {get; set;}
+        public DateTime FechaExpedicion {get; set;}
+        [Range(1,3)]
+        public int Experiencia{get; set;}
+        public bool EnActividad {get; set;}
+    }
+}  
