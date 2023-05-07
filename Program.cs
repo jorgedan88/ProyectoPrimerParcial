5fd248471e655b8a90ef74c09f2b8666b67c7234
jorgedan88 <129257560+jorgedan88@users.noreply.github.com>
2023-05-07 13:30:37 -0300

    Se crean los controlers junto con las vistas correspondientes para los modelos

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ProyectoPrimerParciall.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<InstructorContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("InstructorContext") ?? throw new InvalidOperationException("Connection string 'InstructorContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
