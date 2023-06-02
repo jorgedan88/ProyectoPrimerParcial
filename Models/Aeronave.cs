10c1c8198954f99e5d45d5f5d7af7469f47b3862
jorgedan88 <129257560+jorgedan88@users.noreply.github.com>
2023-06-02 17:03:37 -0300

    Se agrega atributo FechaFabricacion y se modifican las vistas relacionadas

--- Models/Aeronave.cs
+++ Models/Aeronave.cs
@@ -6,11 +6,13 @@ namespace ProyectoPrimerParcial.Models
     {
         public int AeronaveId { get; set; }
 
+        [Display(Name = "Fecha de fabricacion")]
+        [Required(ErrorMessage ="Debe ingresar la fecha de fabricacion")]
+        public DateTime FechaFabricacion {get;set;}
+
         [Required(ErrorMessage ="Debe ingresar el tipo de aeronave")]
         [Display(Name = "Tipo de aeronave")]
         public string? TipoAeronave {get; set;}
-
-        
         public ICollection<Instructor> InstructorList { get; set; } = new List<Instructor>();
     }
 }
--- Models/Instructor.cs
+++ Models/Instructor.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using ProyectoPrimerParcial2.Utils;
 
 
 namespace ProyectoPrimerParcial.Models
@@ -6,25 +7,34 @@ namespace ProyectoPrimerParcial.Models
     public class Instructor
     {
         public int InstructorId { get; set; }
-        
+
         [Required(ErrorMessage ="Debe ingresar el Nombre del instructor")]
         [Display(Name = "Nombre")]
-
         public string? NombreInstructor {get; set;}
-
+        
         [Required(ErrorMessage ="Debe ingresar el Apellido del instructor")]
         [Display(Name = "Apellido")]
-
         public string? Apellido {get; set;}
 
         [Required(ErrorMessage ="Debe ingresar el DNI del instructor")]
         [Display(Name = "DNI")]
         public int DNI {get; set;}
 
-        [Required(ErrorMessage ="Debe ingresar el Legajo de Vuelo del instructor")]
-        [Display(Name = "Legajo")]
-        public int LegajoVuelo {get; set;}
-        
+        [Required(ErrorMessage ="Debe ingresar el tipo de licencia")]
+        [Display(Name = "Tipo de licencia")]
+        public LicenciaType TipoLicencia { get; set; }
+
+        [Required(ErrorMessage ="Debe ingresar el numero de la licencia")]
+        [Display(Name = "Numero")]
+        public int NumeroLicencia {get; set;}
+
+        [Display(Name = "Fecha expedicion")]
+        public DateTime FechaExpedicion {get;set;}
+
+        [Display(Name = "En actividad")]
+        public bool EnActividad {get;set;} = true;
+
+
         [Display(Name = "Aeronave")]
         public int AeronaveId { get; set; }
         public virtual Aeronave? Aeronave { get; set; }
